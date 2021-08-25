2fc5dd3c3ec179a97d43766a36707bad2479ca38
Vice <vicerust@gmail.com>
2021-08-25 16:30:19 -0400

    Add project files.

---
+++ ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TableFlip
+{
+    public class ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ : Exception
+    {
+        public ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ(string message) : base(message) { } 
+
+        public ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ(string message, Exception innerException) : base(message, innerException) { }
+
+
+        public static void FlipTable()
+        {
+            throw new ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ("ǀノဝᆷဝǀノ ㅗᅳㅗ Everything is fucked");
+        }
+    }
+}

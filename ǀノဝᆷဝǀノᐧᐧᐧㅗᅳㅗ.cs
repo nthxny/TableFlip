25c7772ce6a3b7ccd9c93ea5848a6b492f7cd39f
Vice <vicerust@gmail.com>
2021-08-25 16:34:18 -0400

    ǀノဝᆷဝǀノ ㅗᅳㅗ

--- ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ.cs
+++ ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ.cs
@@ -4,14 +4,14 @@ namespace TableFlip
 {
     public class ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ : Exception
     {
-        public ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ(string message) : base(message) { } 
+        public ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ(string message) : base("ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ " + message) { } 
 
-        public ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ(string message, Exception innerException) : base(message, innerException) { }
+        public ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ(string message, Exception innerException) : base("ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ " + message, innerException) { }
 
 
         public static void FlipTable()
         {
-            throw new ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ("ǀノဝᆷဝǀノ ㅗᅳㅗ Everything is fucked");
+            throw new ǀノဝᆷဝǀノᐧᐧᐧㅗᅳㅗ("Everything is fucked");
         }
     }
 }
